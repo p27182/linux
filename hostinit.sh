f9b4b1c9d9d1e6d18cf18036b769f1139c9006db
DergMcDergerson <p27182@gmail.com>
2023-11-17 06:33:27 -0800

    Update hostinit.sh
    prompt edits

--- hostinit.sh
+++ hostinit.sh
@@ -12,6 +12,7 @@ read -p "install figlet and lolcat?: " yn
 if [ "$yn" == "y" ];
 then
   things="$things figlet lolcat"
+  motd=y
 fi
 
 read -p "install tree, curl, net-tools, & traceroute?: " yn
@@ -32,29 +33,30 @@ fi
 #usermod -s /usr/bin/zsh $USER
 chsh -s $(which zsh) $SUDO_USER
 
-#pull x bit from default motd scripts
-chmod -R -x /etc/update-motd.d/*
+if [ "$motd" == "y" ];
+then
+  #pull x bit from default motd scripts
+  chmod -R -x /etc/update-motd.d/*
+  #change motd target from motd.dynamic to motd
+  sed -i -e 's/motd.dynamic/motd/g' /etc/pam.d/login
+  #comment out noupdate
+  sed -i -e 's/session    optional   pam_motd.so noupdate/#session    optional   pam_motd.so noupdate/g' /etc/pam.d/login
+  #get motd file
+  echo "downloading motd..."
+  wget -q https://raw.githubusercontent.com/p27182/linux/main/00-motd
+  #add x bit and move to the spot...
+  chmod +x 00-motd && mv 00-motd /etc/update-motd.d/00-motd
+fi
 
-#change motd target from motd.dynamic to motd
-sed -i -e 's/motd.dynamic/motd/g' /etc/pam.d/login
-#comment out noupdate
-sed -i -e 's/session    optional   pam_motd.so noupdate/#session    optional   pam_motd.so noupdate/g' /etc/pam.d/login
-
-#get motd file
-echo "downloading motd..."
-wget -q https://raw.githubusercontent.com/p27182/linux/main/00-motd
-
-#add x bit and move to the spot...
-chmod +x 00-motd && mv 00-motd /etc/update-motd.d/00-motd
-
-#disable ipv6
-sysctl -w net.ipv6.conf.all.disable_ipv6=1
-sysctl -w net.ipv6.conf.default.disable_ipv6=1
-sysctl -w net.ipv6.conf.lo.disable_ipv6=1
-sysctl -p
-
-#set tz
-timedatectl set-timezone "America/New_York"
+read -p "ddisable IPv6?: " yn
+if [ "$yn" == "y" ];
+then
+  #disable ipv6
+  sysctl -w net.ipv6.conf.all.disable_ipv6=1
+  sysctl -w net.ipv6.conf.default.disable_ipv6=1
+  sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+  sysctl -p
+fi
 
 #switch user and do some user thangs
 sudo -i -u $SUDO_USER bash << EOF
